539065fa1ecb8368c417ad36edb1fb9fb066ed1a
xNoix <sapdoscofre@gmail.com>
2022-05-08 00:28:08 -0400

    Arrays Added

---
+++ first-ts-app/src/salida.js
@@ -0,0 +1,7 @@
+console.log('Hola mundo!');
+var myString = "Hello World";
+// Arrays
+var StringArray = ["Hola", "mundo", "!"];
+var NumberArray = [1, 2, 3, 4, 5];
+var BooleanArray = [true, false, true];
+var AnyArray = [4, true, 6, false, "Hello", [], {}];
--- first-ts-app/src/salida.ts
+++ first-ts-app/src/salida.ts
@@ -1,1 +1,10 @@
 console.log('Hola mundo!')
+
+var myString: string = "Hello World";
+
+
+// Arrays
+var StringArray:string[] = ["Hola","mundo","!"];
+var NumberArray:number[] = [1,2,3,4,5];
+var BooleanArray:boolean[] = [true, false, true];
+var AnyArray: any[] = [4, true , 6, false, "Hello", [], {}]; 
